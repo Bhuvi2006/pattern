7ebbb50b38a030a096f5cc2a27718a5a57108af5
Bhuvi21 <bhuvijai2006@gmail.com>
2024-07-21 22:10:31 +0530

    Add files via upload

---
+++ Half diamond pattern using C.c
@@ -0,0 +1,23 @@
+//Inverted triangle
+int main()
+{
+    int i,j;
+for (i=1;i<=5;i++)
+{
+    for(j=1;j<=i;j++)
+    {
+        printf(" * ");
+        
+    }
+    printf("\n");
+    
+}
+    for (i=4;i>=1;i--)
+    {
+    for(j=1;j<=i;j++)
+        {
+            printf(" * ");
+        
+        }
+        printf("\n");
+    }
